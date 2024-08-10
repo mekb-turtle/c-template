def61bfb6a7cd7e91d5eab82a275639edb58d304
mekb <mekbgithub@icloud.com>
2024-08-10 15:43:11 +1000

    :building_construction: Migrate to meson

--- src/main.c
+++ src/main.c
@@ -5,53 +5,56 @@
 #include <stdbool.h>
 #define eprintf(...) fprintf(stderr, __VA_ARGS__)
 
-static struct option options_getopt[] = {
-        {"help",    no_argument, 0, 'h'},
-        {"version", no_argument, 0, 'V'},
-        {"a",       no_argument, 0, 'a'},
-        {"b",       no_argument, 0, 'b'},
-        {"c",       no_argument, 0, 'c'},
-        {0,         0,           0, 0  }
-};
+// simple program with argument handling
 
 int main(int argc, char *argv[]) {
 	bool invalid = false;
 	int opt;
 
 	// argument handling
-	while ((opt = getopt_long(argc, argv, ":hVabc", options_getopt, NULL)) != -1) {
-		switch (opt) {
-			case 'h':
-				printf("Usage: %s [OPTION]...\n", TARGET);
-				printf("-h --help: Shows help text\n");
-				printf("-V --version: Shows the version\n");
-				printf("-a --a: Do something\n");
-				printf("-b --b: Do something else\n");
-				printf("-c --c: Do another thing\n");
-				return 0;
-			case 'V':
-				printf("%s %s\n", TARGET, VERSION);
-				return 0;
-			default:
-				if (!invalid) {
-					switch (opt) {
-						case 'a':
-							printf("A\n");
-							break;
-						case 'b':
-							printf("B\n");
-							break;
-						case 'c':
-							printf("C\n");
-							break;
-						default:
-							invalid = true;
-							break;
+	while ((opt = getopt_long(argc, argv, ":hVabc", (struct option[]){
+	                {"help",    no_argument, 0, 'h'},
+	                {"version", no_argument, 0, 'V'},
+	                {"a",       no_argument, 0, 'a'},
+	                {"b",       no_argument, 0, 'b'},
+	                {"c",       no_argument, 0, 'c'},
+	                {0,         0,           0, 0  }
+    }, NULL)) != -1) {
+			switch (opt) {
+				case 'h':
+					printf("Usage: %s [OPTION]...\n", PROJECT_NAME);
+					printf("-h --help: Shows help text\n");
+					printf("-V --version: Shows the version\n");
+					printf("-a --a: Do something\n");
+					printf("-b --b: Do something else\n");
+					printf("-c --c: Do another thing\n");
+					return 0;
+				case 'V':
+					printf("%s %s\n", PROJECT_NAME, PROJECT_VERSION);
+#ifdef PROJECT_URL
+					printf("See more at %s\n", PROJECT_URL);
+#endif
+					return 0;
+				default:
+					if (!invalid) {
+						switch (opt) {
+							case 'a':
+								printf("A\n");
+								break;
+							case 'b':
+								printf("B\n");
+								break;
+							case 'c':
+								printf("C\n");
+								break;
+							default:
+								invalid = true;
+								break;
+						}
 					}
-				}
-				break;
+					break;
+			}
 		}
-	}
 
 	if (optind != argc || invalid) {
 		// use `argc - 1` if you want to require one non-flag argument, such as a file name
